62de8213918300ce0e5b370c5102ddd7fa73737e
HanlinMiao <hanlin980505@gmail.com>
2025-03-03 15:34:12 -0500

    -mstarter files

---
+++ src/main/java/com/example/dynasty_fantasy/DynastyFantasyApplication.java
@@ -0,0 +1,13 @@
+package com.example.dynasty_fantasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynastyFantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynastyFantasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dynasty_fantasy/DynastyFantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynasty_fantasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynastyFantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
